927e1744767f6caf64dc5fe98a46168a2a5d5858
mikezangus <zangusm@gmail.com>
2024-02-12 21:25:19 -0500

    nextjs

--- site/src/components/fetchResult.js
+++ site/src/components/fetchResult.js
@@ -1,11 +1,12 @@
 export default async function fetchResult(input, inputType, handleResult) {
     try {
+        const baseURL = process.env.NEXT_PUBLIC_API_BASE_URL || "";
         const params = new URLSearchParams(
             inputType === "ZIP"
                 ? `zip=${input}`
                 : `lat=${input.split(",")[0]}&lon=${input.split(",")[1]}`
         );
-        const url = `http://localhost:4001/client?${params.toString()}`;
+        const url = `${baseURL}/api/client?${params}`;
         const response = await fetch(url);
         if (!response.ok) throw new Error("Network response was not ok");
         const rawOutput = await response.json();
