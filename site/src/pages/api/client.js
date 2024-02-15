b9579fc68d7dc24e109d3a55fb3be8104e185bad
mikezangus <zangusm@gmail.com>
2024-02-15 17:58:43 -0500

    nevermind i don't need cors

--- site/src/pages/api/client.js
+++ site/src/pages/api/client.js
@@ -3,10 +3,6 @@ import connectToMongo from "../../lib/mongoClient";
 
 export default async function handler(req, res) {
 
-    res.setHeader("Access-Control-Allow-Origin", "*");
-    res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
-    res.setHeader("Access-Control-Allow-Headers", "Content-Type");
-
 
     let { zip, lat, lon } = req.query;
     let query = {};
