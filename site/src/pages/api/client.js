314016961a94651d1a1682fc2380b4d3364e31f3
mikezangus <zangusm@gmail.com>
2024-02-15 16:09:11 -0500

    cors fix attempt

--- site/src/pages/api/client.js
+++ site/src/pages/api/client.js
@@ -3,6 +3,11 @@ import connectToMongo from "../../lib/mongoClient";
 
 export default async function handler(req, res) {
 
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
+    res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+
+
     let { zip, lat, lon } = req.query;
     let query = {};
     let pipeline = [];
