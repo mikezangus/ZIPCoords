b8d0d4998e7ba85fd6c458131c9ee2f6fcc99e10
mikezangus <zangusm@gmail.com>
2024-02-15 19:38:05 -0500

    console logs

--- site/src/lib/mongoClient.js
+++ site/src/lib/mongoClient.js
@@ -8,19 +8,20 @@ let cachedDB = null;
 
 export default async function connectToMongo() {
     if (cachedDB) {
-        console.log("Cached DB exists")
+        console.log("Cached DB exists");
         return cachedDB;
     }
     try {
         const client = new MongoClient(uri);
         await client.connect();
         cachedDB = client.db(dbName);
-        console.log("Connected to mongo")
+        console.log("Connected to DB");
         return cachedDB;
     }
     catch (error) {
-        console.error("Failed to connect to database", error);
-        throw new Error("Failed to connect to database")
+        console.log("Failed to connect to DB", error);
+        console.error("Failed to connect to DB", error);
+        throw new Error("Failed to connect to DB");
     }
 
 }
